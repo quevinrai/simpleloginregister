19c43a343fe1055d002d5cc9c105765eea8d03da
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-17 21:29:16 -0400

    Test commit

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# Create your views here
 def home(request):
     return redirect('login')
 
