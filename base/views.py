d071c14dcb4585447279a5dd7c14f76f6ba68831
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-17 21:28:52 -0400

    First commit

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def home(request):
+    return redirect('login')
+
+def login(request):
+    if request.method == "POST":
+        print(request.POST.get('username'))
+        print(request.POST.get('password'))
+        print(request.POST.get('remember_me'))
+
+    return render(request, 'base/login.html')
+
+def register(request):
+    form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'base/register.html', context)
