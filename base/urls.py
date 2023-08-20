fc4d2e2b9f3a999214ccb82edc9c1a0b37e744aa
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-19 21:55:38 -0400

    Update views function names • Replace 'login' with 'login_user' • Replace 'register' with 'register_user'

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
+    path('login', views.login_user, name='login'),
+    path('register', views.register_user, name='register'),
 ]
