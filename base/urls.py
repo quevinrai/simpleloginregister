7e57a8fa574c3f589a3a2385e65f9944da0fc395
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-19 22:31:45 -0400

    Add 'change_password' path

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login_user, name='login'),
     path('register', views.register_user, name='register'),
+    path('change-password', views.change_password, name='change-password'),
     path('logout', views.logout_user, name='logout'),
 ]
