7dc48550c0d6331f986f2166ae4e3784e49527c6
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-23 23:30:59 -0400

    Change User to CustomUser • Add custom fields

--- base/models.py
+++ base/models.py
@@ -2,5 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    pass
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = [username, email, password]
+
+    def __str__(self):
+        return self.username
