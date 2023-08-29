5a78c2b750725fb928ddd7dfec2306f43e834717
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-28 22:31:46 -0400

    Update CustomUser model • Add help_text option to all fields • Change username field max_length=150 • Add validators option to email & password fields

--- base/models.py
+++ base/models.py
@@ -1,13 +1,40 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, EmailValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=128)
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'
+    )
+    first_name = models.CharField(
+        max_length=50,
+        blank=True,
+        help_text='Optional.'
+    )
+    last_name = models.CharField(
+        max_length=50,
+        blank=True,
+        help_text='Optional.'
+    )
+    email = models.CharField(
+        max_length=255,
+        unique=True,
+        validators=[EmailValidator('Required. Type a valid address.')],
+        help_text='Required. Type a valid address.'
+    )
+    password = models.CharField(
+        max_length=128,
+        validators=[MinLengthValidator(4)],
+        help_text='<ul>' +
+            '<li>Your password can\'t be too to your other personal information.</li>' +
+            '<li>Your password must contain at least 8 characters.</li>' +
+            '<li>Your password can\'t be a commonly used password.</li>' +
+            '<li>Your password can\'t be entirely numeric.</li>' +
+        '</ul>'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
