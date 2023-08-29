09b445a7172e6c04e291a2348e642a2772e0f2fe
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-29 08:48:37 -0400

    Create 'change-email' path

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register', views.register_user, name='register'),
     path('change-password', views.change_password, name='change-password'),
     path('change-profile', views.change_profile, name='change-profile'),
+    path('change-email', views.change_email, name='change-email'),
     path('logout', views.logout_user, name='logout'),
 ]
