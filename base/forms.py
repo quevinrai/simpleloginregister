1a27b830dd2aab549afc3a6e1e4101cc978d37da
Quevin Bambasi <quevinbambasi@pm.me>
2023-08-28 21:04:02 -0400

    Import ModelForm • Create ChangeProfileForm class extending ModelForm

--- base/forms.py
+++ base/forms.py
@@ -1,3 +1,4 @@
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
@@ -5,3 +6,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class ChangeProfileForm(ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name']
